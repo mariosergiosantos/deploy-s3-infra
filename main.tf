72a2d9966a4b7a73b281350dde668385257baa0f
Mário Sérgio <mariosergio952@hotmail.com.br>
2024-03-28 14:26:16 -0300

    Remoção das tags

--- main.tf
+++ main.tf
@@ -17,3 +17,30 @@ resource "aws_s3_bucket_website_configuration" "deploy_s3_demo_website" {
     key = "error.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.deploy_s3_demo.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "Permissions S3",
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketTagging",
+          "s3:GetBucketWebsite",
+          "s3:CreateBucket",
+          "s3:DeleteBucketWebsite",
+          "s3:DeleteBucket"
+        ],
+        Resource = [
+          "${aws_s3_bucket.deploy_s3_demo.arn}/*"
+        ]
+      }
+    ]
+  })
+}
